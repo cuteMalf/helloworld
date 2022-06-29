0799aca5f783be5b5c7f916ad27dd55ecdbf0d99
cutema <cutemalinfei@outlook.com>
2022-06-29 20:51:31 +0800

    hot-fix commit 20220629205100

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -6,5 +6,6 @@ public class Demo {
         System.out.println("Hello Maven");
         System.out.println("好好学习 天天向上");
         System.out.println("我是hot-fix");
+        System.out.println("hot-fix 第二次提交");
     }
 }
